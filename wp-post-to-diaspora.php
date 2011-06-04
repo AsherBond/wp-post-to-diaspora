28f9b01f3e6f83ad521a558a642d9deffcd456c9
Joe Bivins <jbivins@gmail.com>
2011-06-04 03:04:28 -0400

    Added link shortener choices

--- wp-post-to-diaspora.php
+++ wp-post-to-diaspora.php
@@ -63,7 +63,22 @@
 			'type'		=> 'checkbox',
 			'options'	=> array (
 						array( 'label'	=> 'Encrypted',
-						       'value'	=> Diaspora::HTTPS ),
+						       'value'	=> Diaspora::HTTPS )
+			)
+		);
+
+		$field_args_by_id['url_shortener'] = array(
+			'default_value' => 'tinyurl.com',
+			'label'         => 'Link Shortener',
+			'name'          => 'url_shortener',
+			'type'          => 'select',
+			'options'       => array (
+						array( 'label'  => 'TinyURL',
+						       'value'  => 'tinyurl.com' ),
+						array( 'label'  => 'Goo.gl',
+						       'value'  => 'goo.gl' ),
+						array( 'label'  => 'Is.gd',
+						       'value'  => 'is.gd' )
 			)
 		);
 
@@ -107,6 +122,30 @@
 
 					echo "<input id='{$args['id']}' class='{$args['class']}' name='wp_post_to_diaspora_options[{$args['name']}][]' type='{$args['type']}' value='{$option['value']}' $checked /> {$option['label']}";
 				}
+				break;
+			case 'select':
+			case 'select-one':
+				echo "<select id='{$args['id']}' class='{$args['class']}' name='wp_post_to_diaspora_options[{$args['name']}][]'>";
+
+				foreach ($args['options'] as $option) {
+					$selected = '';
+
+					if ( is_array( $value ) ) {
+						if ( in_array($option['value'], $value ) ) {
+							$selected = "selected='selected'";
+						}
+					}
+					else if ( $option['value'] == $value ) {
+						$selected = "selected='selected'";
+					}
+
+					echo "<option value='{$option['value']}' $selected>{$option['label']}</option>";
+
+				}
+
+				echo "</select>";
+				break;
+
 			default:
 			
 				break;
@@ -137,14 +176,17 @@
 	}
 
 	function wp_post_to_diaspora_process_content($content) {
+		$options = get_option ( 'wp_post_to_diaspora_options' );
+
+		$url_shortener  = $options['url_shortener'];
 		$pattern = '/(?#Protocol)(?:(?:ht|f)tp(?:s?)\:\/\/|~\/|\/)?(?#Username:Password)(?:\w+:\w+@)?(?#Subdomains)(?:(?:[-\w]+\.)+(?#TopLevel Domains)(?:com|org|net|gov|mil|biz|info|mobi|name|aero|jobs|museum|travel|[a-z]{2}))(?#Port)(?::[\d]{1,5})?(?#Directories)(?:(?:(?:\/(?:[-\w~!$+|.,=]|%[a-f\d]{2})+)+|\/)+|\?|#)?(?#Query)(?:(?:\?(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)(?:&(?:[-\w~!$+|.,*:]|%[a-f\d{2}])+=?(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)*)*(?#Anchor)(?:#(?:[-\w~!$+|.,*:=]|%[a-f\d]{2})*)?/';
 		preg_match_all($pattern, $content, $matches);
 
 		if ((isset($matches[0])) && (is_array($matches[0]))) {
 			foreach ($matches[0] as $match) {
-				$shortened_url = wp_post_to_diaspora_shrink_url($match);
+				$shortened_url = wp_post_to_diaspora_shrink_url($url_shortener, $match);
 				if ($shortened_url !== false) {
-					$content = str_replace($match, wp_post_to_diaspora_shrink_url($match), $content);
+					$content = str_replace($match, wp_post_to_diaspora_shrink_url($url_shortener, $match), $content);
 				}
 			}
 		}
@@ -157,14 +199,28 @@
 
 		die();
 	}
-	
-	function wp_post_to_diaspora_shrink_url ($url) {
-		$target = 'http://tinyurl.com/api-create.php?url=';
+
+	function wp_post_to_diaspora_shrink_url($url_shortener, $url) {
+		switch ($url_shortener) {
+			case 'goo.gl':
+				$url = wp_post_to_diaspora_shrink_url_googl($url);
+				break;
+			case 'is.gd':
+				$url = wp_post_to_diaspora_shrink_url_http_get('http://is.gd/create.php?format=simple&url=', $url);
+				break;
+			default:
+				$url = wp_post_to_diaspora_shrink_url_http_get('http://tinyurl.com/api-create.php?url=', $url);
+				break;
+		}
+
+		return $url;
+	}
+
+	function wp_post_to_diaspora_shrink_url_http_get($end_point, $url) {
 		$ch    = curl_init();
-		$url   = false;
 
 		if ($ch !== false) {
-			curl_setopt($ch, CURLOPT_URL, $target . $url);
+			curl_setopt($ch, CURLOPT_URL, $end_point . $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 			$url = curl_exec($ch);
@@ -173,7 +229,36 @@
 
 		return $url;
 	}
-	
+
+	function wp_post_to_diaspora_shrink_url_googl($url) {
+		$target = 'https://www.googleapis.com/urlshortener/v1/url';
+
+		$ch             = curl_init();
+		$response       = null;
+
+		if ($ch !== false) {
+			curl_setopt($ch, CURLOPT_URL, $target);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, '{"longUrl": "' . $url . '"}');
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+			$response = curl_exec($ch);
+			curl_close($ch);
+		}
+
+		if ($response !== false) {
+			$json_response = json_decode($response, true);
+
+			if (($json_response !== false) && (isset($json_response['id']))) {
+				$url = $json_response['id'];
+			}
+		}
+
+		return $url;
+	}
+
 	function wp_post_to_diaspora_add_admin_page () {
 		add_options_page ('Post To diaspora', 'Post To diaspora', 'manage_options', 'wp-post-to-diaspora', 'wp_post_to_diaspora_options');
 	}
@@ -193,14 +278,16 @@
 			require_once dirname(__FILE__) . '/diaspora.php';
 			$options = get_option ( 'wp_post_to_diaspora_options' );
 
-			$handle = $options['handle'];
-			$pass = $options['password'];
-			$protocol = $options['protocol'];
+			$handle		= $options['handle'];
+			$pass		= $options['password'];
+			$protocol	= $options['protocol'];
+			$url_shortener  = $options['url_shortener'];
+
 			$post = get_post ($postID);
 			$str = '%s - %s';
 			$permalink = get_permalink($postID);
 
-			$shortened_url = wp_post_to_diaspora_shrink_url($permalink);
+			$shortened_url = wp_post_to_diaspora_shrink_url($url_shortener, $permalink);
 			if ($shortened_url !== false) {
 				$content = sprintf($str, $post->post_title, $shortened_url);
 			}
