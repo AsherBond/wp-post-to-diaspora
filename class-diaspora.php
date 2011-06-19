f72ca961f0e6d1f4c5433389b7b927e6489240bf
Joe Bivins <jbivins@gmail.com>
2011-06-19 16:50:03 -0400

    Bugfix.  Don't try to post to Diaspora when a username/server name is missing.  Sends the 'password' using the correct property.

--- class-diaspora.php
+++ class-diaspora.php
@@ -76,52 +76,53 @@ class Diaspora {
 		if ( ( empty( $this->username ) ) || ( empty( $this->server_domain ) ) ) {
 			$diaspora_status = 'Error posting to Diaspora.  Please use your full Diaspora Handle in the form of username@server_name.com';
 		}
+		else {
+			$json_array = array('auth_token' => $this->password, 'text' => $processed_message, 'format' => 'json');
+			$json_string = json_encode($json_array);
+			$host = $this->protocol . '://' . $this->server_domain . '/activity_streams/notes.json';
 
-		$json_array = array('auth_token' => $password, 'text' => $processed_message, 'format' => 'json');
-		$json_string = json_encode($json_array);
-		$host = $this->protocol . '://' . $this->server_domain . '/activity_streams/notes.json';
+			$resultArray = null;
+			$diaspora_status = '';
 
-		$resultArray = null;
-		$diaspora_status = '';
+			$ch = curl_init();
 
-		$ch = curl_init();
+			if ($ch !== false) {
+				curl_setopt_array($ch, array(
+					CURLOPT_CONNECTTIMEOUT => 30,
+					CURLOPT_URL => $host,
+					CURLOPT_VERBOSE => 1,
+					CURLOPT_RETURNTRANSFER => 1,
+					//CURLOPT_USERPWD => "$username:$password",
+					CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+					CURLOPT_POST => 1,
+					CURLOPT_HTTPHEADER => array('Content-type: application/json'),
+					CURLOPT_POSTFIELDS => $json_string
+				));
 
-		if ($ch !== false) {
-			curl_setopt_array($ch, array(
-				CURLOPT_CONNECTTIMEOUT => 30,
-				CURLOPT_URL => $host,
-				CURLOPT_VERBOSE => 1,
-				CURLOPT_RETURNTRANSFER => 1,
-				//CURLOPT_USERPWD => "$username:$password",
-				CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-				CURLOPT_POST => 1,
-				CURLOPT_HTTPHEADER => array('Content-type: application/json'),
-				CURLOPT_POSTFIELDS => $json_string
-			));
+				$result = curl_exec($ch);
+				if ($result !== false) {
+					$resultArray = curl_getinfo($ch);
+				}
+				else {
+					$diaspora_status = 'Error posting to Diaspora. Error Code: ' . curl_error($ch);
+				}
 
-			$result = curl_exec($ch);
-			if ($result !== false) {
-				$resultArray = curl_getinfo($ch);
+				curl_close($ch);
 			}
 			else {
-				$diaspora_status = 'Error posting to Diaspora. Error Code: ' . curl_error($ch);
+				$diaspora_status = 'Error creating a cURL resource.';
 			}
 
-			curl_close($ch);
-		}
-		else {
-			$diaspora_status = 'Error creating a cURL resource.';
-		}
-
-		if (is_array($resultArray)) {
-			if ($resultArray['http_code'] == "200") {
-				$diaspora_status='You just posted!';
-			} else {
-				$diaspora_status="Error posting to Diaspora. Retry Code: {$resultArray['http_code']}";
+			if (is_array($resultArray)) {
+				if ($resultArray['http_code'] == "200") {
+					$diaspora_status='You just posted!';
+				} else {
+					$diaspora_status="Error posting to Diaspora. Retry Code: {$resultArray['http_code']}";
+				}
 			}
-		}
 
-		return $diaspora_status;
+			return $diaspora_status;
+		}
 	}
 
 }
