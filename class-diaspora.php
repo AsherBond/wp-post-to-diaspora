c25b2f7ad446e3d3c5d4f80ba0ad716a351763f0
Joe Bivins <jbivins@gmail.com>
2011-06-19 16:45:53 -0400

    Add test for posting to Diaspora (incomplete).

--- class-diaspora.php
+++ class-diaspora.php
@@ -76,46 +76,47 @@ class Diaspora {
 		if ( ( empty( $this->username ) ) || ( empty( $this->server_domain ) ) ) {
 			$diaspora_status = 'Error posting to Diaspora.  Please use your full Diaspora Handle in the form of username@server_name.com';
 		}
+		else {
+			$json_array = array('auth_token' => $this->password, 'text' => $processed_message, 'format' => 'json');
+			$json_string = json_encode($json_array);
+			$host = $this->protocol . '://' . $this->server_domain . '/activity_streams/notes.json';
 
-		$json_array = array('auth_token' => $password, 'text' => $processed_message, 'format' => 'json');
-		$json_string = json_encode($json_array);
-		$host = $this->protocol . '://' . $this->server_domain . '/activity_streams/notes.json';
+			$resultArray = null;
+			$diaspora_status = '';
 
-		$resultArray = null;
-		$diaspora_status = '';
+			$ch = curl_init();
 
-		$ch = curl_init();
+			if ($ch !== false) {
+				curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+				curl_setopt($ch, CURLOPT_URL, $host);
+				curl_setopt($ch, CURLOPT_VERBOSE, 1);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				//curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
+				curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+				curl_setopt($ch, CURLOPT_POST, 1);
+				curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
+				curl_setopt($ch, CURLOPT_POSTFIELDS, $json_string);
 
-		if ($ch !== false) {
-			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-			curl_setopt($ch, CURLOPT_URL, $host);
-			curl_setopt($ch, CURLOPT_VERBOSE, 1);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			//curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-			curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-			curl_setopt($ch, CURLOPT_POST, 1);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json'));
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $json_string);
+				$result = curl_exec($ch);
+				if ($result !== false) {
+					$resultArray = curl_getinfo($ch);
+				}
+				else {
+					$diaspora_status = 'Error posting to Diaspora. Error Code: ' . curl_error($ch);
+				}
 
-			$result = curl_exec($ch);
-			if ($result !== false) {
-				$resultArray = curl_getinfo($ch);
+				curl_close($ch);
 			}
 			else {
-				$diaspora_status = 'Error posting to Diaspora. Error Code: ' . curl_error($ch);
+				$diaspora_status = 'Error creating a cURL resource.';
 			}
 
-			curl_close($ch);
-		}
-		else {
-			$diaspora_status = 'Error creating a cURL resource.';
-		}
-
-		if (is_array($resultArray)) {
-			if ($resultArray['http_code'] == "200") {
-				$diaspora_status='You just posted!';
-			} else {
-				$diaspora_status="Error posting to Diaspora. Retry Code: {$resultArray['http_code']}";
+			if (is_array($resultArray)) {
+				if ($resultArray['http_code'] == "200") {
+					$diaspora_status='You just posted!';
+				} else {
+					$diaspora_status="Error posting to Diaspora. Retry Code: {$resultArray['http_code']}";
+				}
 			}
 		}
 
---
+++ tests/DiasporaTest.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'class-diaspora.php';
+
+class DiasporaTest extends PHPUnit_Framework_TestCase {
+
+	public function testPost() {
+		$diaspora = new Diaspora();
+
+		$response = $diaspora->postToDiaspora();
+		$this->assertContains('Error posting to Diaspora.', $response);
+
+		// @todo handle posting a test message to a Diaspora instance
+	}
+
+	public function testSetHandle() {
+		$diaspora = new Diaspora();
+
+		$diaspora->setHandle( 'test_user@test_server.com' );
+		$this->assertAttributeEquals('test_user', 'username', $diaspora);
+		$this->assertAttributeEquals('test_server.com', 'server_domain', $diaspora);
+
+	}
+
+}
+
+?>
